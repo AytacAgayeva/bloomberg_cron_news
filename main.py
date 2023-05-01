db70f1208b5c17f77fdd651d848ae0bccfc59d5c
Aytage <125976794+Aytage@users.noreply.github.com>
2023-05-01 12:19:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ today=date.today()
 now = datetime.now()
 current_time = now.strftime("%H_%M")
 name=f"https://github.com/Aytage/bloomberg_cron_news/tree/main/xml/sitemap_news_{today}__{current_time}.xml"
-print(name+"hello")
-#filename = wget.download(url, name)
+filename = wget.download(url)
 
 '''with open(filename, 'r',encoding='utf-8') as file:
     xml_content = file.read()
